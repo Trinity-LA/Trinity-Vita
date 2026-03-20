0779ea6c29f8a660f5435816bf9abfbc389aff03
ezequielgk <rox737280@gmail.com>
2026-03-20 00:56:17 -0300

    chore: remove Vercel Analytics integration by deleting Layout.vue and its injection from index.ts.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,20 +2,13 @@ import DefaultTheme from 'vitepress/theme'
 import CommunityCard from './components/CommunityCard.vue'
 import FlatpakButton from './components/FlatpakButton.vue'
 import MinecraftButton from './components/MinecraftButton.vue'
-import { inject } from '@vercel/analytics' // <--- Importamos la función, no el componente
 import './style.css'
 
 export default {
     extends: DefaultTheme,
     enhanceApp({ app }: { app: any }) {
-        // Registramos tus componentes
         app.component('CommunityCard', CommunityCard)
         app.component('FlatpakButton', FlatpakButton)
         app.component('MinecraftButton', MinecraftButton)
-
-        // Ejecutamos la analítica SOLO en el navegador para no romper el build
-        if (typeof window !== 'undefined') {
-            inject()
-        }
     }
 }
